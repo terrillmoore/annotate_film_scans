ac7d7d6e33bf043e3a7b1539a28e02c4ba903a76
Terry Moore <tmm@mcci.com>
2024-06-22 17:33:54 -0400

    Version is 2.5.1-pre1 for development

--- annotate_film_scans/__version__.py
+++ annotate_film_scans/__version__.py
@@ -14,4 +14,4 @@
 ##############################################################################
 
 # this is the version -- setup.cfg uses it, as does __init__.
-__version__ = "2.5.0"
+__version__ = "2.5.1-pre1"
