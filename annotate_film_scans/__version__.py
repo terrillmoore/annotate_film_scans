326c1fc12ee1e9a533e6289b53f56ce570618e12
Terry Moore <tmm@mcci.com>
2025-10-02 21:49:57 -0400

    This is version v2.8.0-pre7

--- annotate_film_scans/__version__.py
+++ annotate_film_scans/__version__.py
@@ -14,4 +14,4 @@
 ##############################################################################
 
 # this is the version -- setup.cfg uses it, as does __init__.
-__version__ = "2.8.0-pre6"
+__version__ = "2.8.0-pre7"
